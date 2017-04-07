ed7fcb36aab2717ffcbc93b608c34780da023b24
Vo1ked <Volk.xxx@mail.ru>
2017-04-07 17:07:52 +0300

    Create Flor,canvas , charter move

---
+++ Assets/Script/PlayerControler.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    [SerializeField]
+    public bool LeftMoving, RightMoving,Moving;
+
+    public float TurnSpeed,Speed;
+
+    private CharacterController _charterMove;
+
+    private Vector3 _forwardmove;
+    // Use this for initialization
+    void Start()
+    {
+        LeftMoving = false;
+        RightMoving = false;
+        Moving = true;
+      //  StartCoroutine(ForwardMove());
+        _charterMove = GetComponent<CharacterController>();
+        _forwardmove = new Vector3(0,-1f,1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _charterMove.Move(_forwardmove * Speed);
+        if (LeftMoving)
+        {
+            _charterMove.Move(Vector3.left * TurnSpeed);
+        }
+        if (RightMoving)
+        {
+            _charterMove.Move(Vector3.right * TurnSpeed);
+        }
+    }
+
+    public void BeginMoveLeft()
+    {
+        LeftMoving = !LeftMoving;
+
+    }
+    public void BeginMoveRight()
+    {
+        RightMoving = !RightMoving;
+    }
+
+
+}
---
+++ Assets/Script/newFloorCreate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;using System.Collections.Generic;using UnityEngine;
+
+public class newFloorCreate : MonoBehaviour
+{
+    public GameObject PrefabFloor;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    for (int i = 1; i < 20; i++)
+	    {
+	        Instantiate(PrefabFloor, new Vector3(0, transform.position.y - 3.4695f * i, transform.position.z + 19.69f*i),
+	            transform.rotation);
+	    }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
